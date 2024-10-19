45f283a7dbd0173206e3f8158e808ace97bdc268
ShubhamPatel2305 <shubhamcp23@gmail.com>
2024-10-19 22:35:55 +0530

    basic interface setup

--- contracts/TokenICO.sol
+++ contracts/TokenICO.sol
@@ -1,3 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+interface ERC20 {
+    function transfer() external returns(bool);
+    function balanceOf() external view returns(uint256);
+    function allowance() external view returns(uint256);
+    function approve() external view returns(bool);
+    function transferFrom() external view returns(bool);
+    //symbol
+    function symbol() external view returns(string memory);
+    //total supply
+    function totalSupply() external view returns(uint256);
+    //name
+    function name() external view returns(string memory);
+    
+}
