a68df803dbc47415d7888f4863a8ce67ea4cbf3a
ShubhamPatel2305 <shubhamcp23@gmail.com>
2024-10-19 23:19:42 +0530

    token deploy script

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -2,3 +2,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+//import openzepelin
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TokenIco is ERC20{
+    constructor() ERC20("TokenIco","@PAPA"){
+        _mint(msg.sender,1000000000000000000); //1 eth total supply
+    }
+}
+
